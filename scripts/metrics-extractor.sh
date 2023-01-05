208d243fd5b8b651bcae854fee31225802f5c658
Victor Amorim <victor.amsantos@gmail.com>
2023-01-05 13:57:39 -0300

    adding common to each element in array

--- scripts/metrics-extractor.sh
+++ scripts/metrics-extractor.sh
@@ -7,17 +7,18 @@ PRETTY_JSON=false
 METRICS_FILE=metrics.json
 OUTPUT_FILE=output.js
 
-curl -XGET -G "${PROMETHEUS_URL}/api/v1/label/__name__/values" \
-  --data-urlencode "match[]={job=\"${TARGET_JOB}\"}" > $METRICS_FILE
-
-if $PRETTY_JSON; then
-  cat <<< $(jq . $METRICS_FILE) > $METRICS_FILE
-fi
+#curl -XGET -G "${PROMETHEUS_URL}/api/v1/label/__name__/values" \
+#  --data-urlencode "match[]={job=\"${TARGET_JOB}\"}" > $METRICS_FILE
+#
+#if $PRETTY_JSON; then
+#  cat <<< $(jq . $METRICS_FILE) > $METRICS_FILE
+#fi
 
 TYPES="histogram gauge"
 JS_EXPORTER=""
 for type in $TYPES; do
   metrics=$(cat $METRICS_FILE | jq -c '.data[] | select(contains("'${type}'"))')
+  metrics=$(sed '$!s/$/,/' <<< $metrics)
   JS_EXPORTER="${type}_metrics,${JS_EXPORTER}"
 
 cat <<EOF >> ${OUTPUT_FILE}
